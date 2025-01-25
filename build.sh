ccda7f761eac15ca8116bc339b70f950769f22d1
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-01-25 15:47:13 +0200

    update build

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-#!/user/bin/env bash
-#Exit on error
-
+#!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Convert static assert files
+# Convert static asset files
 python manage.py collectstatic --no-input
 
-# Apply database migrations
+# Apply any outstanding database migrations
 python manage.py migrate
